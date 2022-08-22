f0490052f4e79c8382f5c3f0679eaaa984637954
Anatoly Popov <ana23123@yandex.ru>
2022-08-23 02:26:53 +0500

    feat: добавить лайки карточкам

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,8 +10,7 @@ module.exports.getCards = (req, res) => {
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  const ownerId = req.user._id;
-  Card.create({ name, link, owner: ownerId })
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => {
       res.status(200).send(card);
     })
@@ -25,3 +24,23 @@ module.exports.deleteCard = (req, res) => {
     })
     .catch(() => res.status(500).send({ message: 'Ошибка на сервере' }));
 };
+
+module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+  { new: true }
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch(() => res.status(500).send({ message: 'Ошибка на сервере' })),
+);
+
+module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
+  req.params.cardId,
+  { $pull: { likes: req.user._id } }, // убрать _id из массива
+  { new: true }
+    .then((card) => {
+      res.status(200).send(card);
+    })
+    .catch(() => res.status(500).send({ message: 'Ошибка на сервере' })),
+);
