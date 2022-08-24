e877d3ebc61b5ca47be1555c999042deaf62d025
Anatoly Popov <ana23123@yandex.ru>
2022-08-24 14:46:46 +0500

    feat: добавить обработку ошибок у карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,66 +1,47 @@
 const Card = require('../models/card');
-const BadRequestError = require('../errors/bad-request-err');
-const NotFoundError = require('../errors/not-found-err');
-const DelCardError = require('../errors/del-card-err');
 
-const { STATUS_OK, ERROR_SERVER } = require('../utils/constants');
+const {
+  ERROR_SERVER,
+  ERROR_NOT_FOUND,
+  ERROR_DEL_CARD,
+  ERROR_BAD_REQUEST,
+} = require('../utils/constants');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => {
-      res.status(STATUS_OK).send(cards);
-    })
+    .then((cards) => res.send({ data: cards }))
     .catch(() => res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => {
-      res.status(STATUS_OK).send(card);
-    })
+    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        throw new BadRequestError('Ошибка валидации данных');
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Ошибка валидации данных' });
       }
     })
     .catch(() => res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' }));
 };
 
-// module.exports.deleteCard = (req, res) => {
-//   const id = req.params;
-
-//   Card.findById(id)
-//     .then((card) => {
-//       if (!card) {
-//         throw new NotFoundError('Такой карточки нет!');
-//       }
-//       if (JSON.stringify(card.owner) !== JSON.stringify(req.user._id)) {
-//         throw new BadRequestError('Невозможно удалить данную карточку');
-//       }
-//       return Card.findByIdAndRemove(id);
-//     })
-//     .then((card) => res.status(STATUS_OK).send(card))
-//     .catch(() => res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' }));
-// };
-
-module.exports.deleteCard = (req, res, next) => {
+module.exports.deleteCard = (req, res) => {
   Card.findById(req.params.cardId)
+    .orFail(() => {
+      res.status(ERROR_NOT_FOUND).send({ message: 'Карточки с таким id не найдено' });
+    })
     .then((card) => {
-      if (!card) {
-        throw new NotFoundError('Карточка с указанным _id не найдена.');
-      } else if (!card.owner.equals(req.user._id)) {
-        throw new DelCardError('Попытка удалить чужую карточку.');
+      if (card.owner.toString() === req.user._id) {
+        Card.findByIdAndRemove({
+          _id: req.params.cardId,
+          owner: req.user._id,
+        })
+          .then((delCard) => res.send({ data: delCard }));
       } else {
-        return card.remove().then(() => res.status(STATUS_OK).send(card));
+        res.status(ERROR_DEL_CARD).send({ message: 'Нельзя удалить чужую карточку' });
       }
     })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return next(new BadRequestError('Переданы некорректные данные при удалении карточки.'));
-      }
-      return next(err);
-    });
+    .catch(() => res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.likeCard = (req, res) => {
@@ -71,9 +52,9 @@ module.exports.likeCard = (req, res) => {
   )
     .then((card) => {
       if (!card) {
-        throw new NotFoundError('Такой карточки нет!');
+        res.status(ERROR_NOT_FOUND).send({ message: 'Карточки с таким id не найдено' });
       }
-      res.status(STATUS_OK).send(card);
+      res.send(card);
     })
     .catch(() => res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' }));
 };
@@ -86,9 +67,9 @@ module.exports.dislikeCard = (req, res) => {
   )
     .then((card) => {
       if (!card) {
-        throw new NotFoundError('Такой карточки нет!');
+        res.status(ERROR_NOT_FOUND).send({ message: 'Карточки с таким id не найдено' });
       }
-      res.status(STATUS_OK).send(card);
+      res.send({ data: card });
     })
     .catch(() => res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' }));
 };
