e5f03b9911b21f327c94942eef30156bf6887992
Anatoly Popov <ana23123@yandex.ru>
2022-08-30 17:57:20 +0500

    feat: добавить функционал

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '630235efe2ee3e5a9fdd5635',
+  };
+
+  next();
+});
+
 app.use('/', users);
 app.use('/', cards);
 
