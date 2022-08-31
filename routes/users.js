3965991fc81f6c71398071c236a65a47f488c8cd
Anatoly Popov <ana23123@yandex.ru>
2022-08-31 20:01:36 +0500

    fix: дополнить routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const users = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers,
@@ -11,7 +12,15 @@ const {
 users.get('/users', getUsers);
 users.get('/users/:userId', getUsersById);
 users.get('/users/me', getCurrentUser);
-users.patch('/users/me', updateProfile);
+// users.patch('/users/me', updateProfile);
+
+users.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
+
 users.patch('/users/me/avatar', updateAvatar);
 
 module.exports = users;
